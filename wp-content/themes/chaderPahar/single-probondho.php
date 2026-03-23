f29eeb2b9ef560f0eb04f6ff357d95a61923fd03
donald <donald@w.mediaplanetus.com>
2026-03-23 14:52:47 +0600

    Remove excerpt display from author section in 'single-probondho.php'

--- wp-content/themes/chaderPahar/single-probondho.php
+++ wp-content/themes/chaderPahar/single-probondho.php
@@ -26,7 +26,6 @@
                 <?php if ($custom_author) : ?>
                 <p class="post-author-name"><?php echo esc_html($custom_author); ?></p>
                 <?php endif; ?>
-                <?php the_excerpt(); ?>
             </div>
           </div>
 
