400553cce5b30a7d8cb8dd6708854149555e3bea
donald <donald@w.mediaplanetus.com>
2026-03-24 21:19:57 +0600

    Comment out author name display in single kobita template

--- wp-content/themes/chaderPahar/single-kobita.php
+++ wp-content/themes/chaderPahar/single-kobita.php
@@ -24,7 +24,7 @@
                 <!-- <h2><?//php the_title(); ?></h2> -->
                 <?php $custom_author = get_post_meta(get_the_ID(), '_custom_author', true); ?>
                 <?php if ($custom_author) : ?>
-                <p class="post-author-name"><?php echo esc_html($custom_author); ?></p>
+                <!-- <p class="post-author-name"><?//php echo esc_html($custom_author); ?></p> -->
                 <?php endif; ?>
                 <?php echo category_description(); ?>
                 <?php the_content(); ?>
